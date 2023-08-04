7598301c2e55c8e060a10ee51787663f81b78954
Amlan101 <amlan.sahooricky@gmail.com>
2023-08-04 07:26:36 +0530

    Deployed the Django App on Vercel

--- animeList/settings.py
+++ animeList/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-dj5qjfiz*jlxht!3hbbpwyc-=&&(@f!c4!tq4kw^25%s#+jw_1
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['.vercel.app']
 
 
 # Application definition
