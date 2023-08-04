21c59923d350af5608079fc46c937b0bf3e4e5d7
Amlan101 <amlan.sahooricky@gmail.com>
2023-08-04 08:14:20 +0530

    Modified the settings.py file

--- animeList/settings.py
+++ animeList/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-dj5qjfiz*jlxht!3hbbpwyc-=&&(@f!c4!tq4kw^25%s#+jw_1
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['.vercel.app']
+ALLOWED_HOSTS = ['.vercel.app','127.0.0.1']
 
 
 # Application definition
